59bf694ca697f887f0cf801ec6c51e545e99224d
Yurii Khvishchuk <owngameplay@gmail.com>
2023-08-10 00:29:03 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.(.*)$/,
+        use: [
+          {
+            loader: 'raw-loader',
+            options: {
+              name: '[name].[ext]',
+            },
+          },
+        ],
+        include: path.resolve(__dirname, './database'), 
+      }
+    ],
+  }
+};
