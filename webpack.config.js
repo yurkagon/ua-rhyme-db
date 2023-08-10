f80732964ae966536f8c238a042155723b3b367a
Yurii Khvishchuk <owngameplay@gmail.com>
2023-08-10 13:35:37 +0300

    Fixed devserver

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
+    publicPath: '/'
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
@@ -30,5 +31,13 @@ module.exports = {
         type: 'asset/source',
       },
     ],
-  }
+  },
+  devServer: {
+    port: 3000,
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    historyApiFallback: true,
+  },
 };
