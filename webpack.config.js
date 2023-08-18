fed090c7a2383727c107fe1237f68e32e127236a
Yurii Khvishchuk <owngameplay@gmail.com>
2023-08-18 22:32:13 +0300

    Fixed deployment

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "public/index.html",
+      publicPath: require("./package.json").homepage,
     }),
   ],
   module: {
